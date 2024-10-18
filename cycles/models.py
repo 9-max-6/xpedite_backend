f4797f3d73866fe6727bf5b805adba37dc32400d
9-max-6 <mutukumaxwel@gmail.com>
2024-10-18 10:16:09 +0300

    Feat: added the autonow_add parameter of the Super cycle and cycle model to only note the time when the model is created.

--- cycles/models.py
+++ cycles/models.py
@@ -6,14 +6,14 @@ from users.models import CustomUser
 class SuperCycle(models.Model):
     """super cycle"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.TimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(blank=False, max_length=255)
 
 
 class Cycle(models.Model):
     """cycle"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.TimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='cycles')
     supercycle = models.ForeignKey(SuperCycle, on_delete=models.CASCADE, related_name='cycles')
     status = models.BooleanField(default=False)
--- cycles/views.py
+++ cycles/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from requests.permissions import IsManagement
+from rest_framework.permissions import IsAuthenticated
 from .models import SuperCycle, Cycle
 from requests.models import Request
 from .serializers import SuperCyCleSerializer, CycleSerializer
@@ -7,14 +8,18 @@ from rest_framework.decorators import action
 
 class SuperCycleViewSet(viewsets.ModelViewSet):
     """Cycles"""
-    queryset = SuperCycle.objects.all()
-    permission_classes = [IsManagement]
+    queryset = SuperCycle.objects.order_by('-created_at').all()
+    permission_classes = [IsAuthenticated, IsManagement]
     serializer_class = SuperCyCleSerializer
 
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return [IsAuthenticated()]
+        return super().get_permissions()
 
 class CycleViewSet(viewsets.ModelViewSet):
     """Cycles"""
-    permission_classes = [IsManagement]
+    permission_classes = [IsManagement, IsAuthenticated]
     serializer_class = CycleSerializer
     queryset = Cycle.objects.all()
 
