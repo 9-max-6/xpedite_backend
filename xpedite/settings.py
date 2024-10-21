596c6db0c1f187f988a456dd84f2d0025ada29ca
9-max-6 <mutukumaxwel@gmail.com>
2024-10-21 11:19:59 +0300

    Fix: fixed spelling error in get env function of the os module

--- xpedite/settings.py
+++ xpedite/settings.py
@@ -30,7 +30,7 @@ django_heroku.settings(locals(), staticfiles=False)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenviron('debug', True)
+DEBUG = os.getenv('debug', True)
 
 ALLOWED_HOSTS = ['xpedite-1487e9d7deba.herokuapp.com', '127.0.0.1', 'localhost']
 
